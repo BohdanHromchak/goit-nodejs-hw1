92021b7e7a44c005fc27a076da5dc6cdce1526ca
BohdanHromchak <gromchakbogdan@gmail.com>
2023-05-13 19:19:59 +0300

    add commander

--- contacts.js
+++ contacts.js
@@ -15,14 +15,13 @@ const listContacts = async () => {
 
   // const data = await fs.readFile(contactsPath, "utf-8")
   // console.log(data)
-
   return JSON.parse(data);
 };
 
 const getContactById = async (contactId) => {
   const contacts = await listContacts();
   const result = contacts.find((item) => item.id === contactId);
-  return result || null;
+  return result;
 };
 
 const removeContact = async (contactId) => {
