5eafb24f2f48043a687872ca813f02428afdcd95
BohdanHromchak <gromchakbogdan@gmail.com>
2023-05-13 17:01:42 +0300

    add files

---
+++ contacts.js
@@ -0,0 +1,27 @@
+/*
+ * Розкоментуйте і запиши значення
+ * const contactsPath = ;
+ */
+
+const fs = require("fs/promises");
+const path = require("path");
+
+const contactsPath = path.join(__dirname, "db", "movies.json");
+console.log(contactsPath)
+
+// TODO: задокументувати кожну функцію
+function listContacts() {
+    // ...твій код
+  }
+  
+  function getContactById(contactId) {
+    // ...твій код
+  }
+  
+  function removeContact(contactId) {
+    // ...твій код
+  }
+  
+  function addContact(name, email, phone) {
+    // ...твій код
+  }
