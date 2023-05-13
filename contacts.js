99189db37d1356400893f9868edcd542bba9f4c8
BohdanHromchak <gromchakbogdan@gmail.com>
2023-05-13 18:47:38 +0300

    add functions

--- contacts.js
+++ contacts.js
@@ -1,27 +1,58 @@
-/*
- * Розкоментуйте і запиши значення
- * const contactsPath = ;
- */
-
 const fs = require("fs/promises");
+const { nanoid } = require("nanoid");
 const path = require("path");
 
-const contactsPath = path.join(__dirname, "db", "movies.json");
-console.log(contactsPath)
+const contactsPath = path.join(__dirname, "db", "contacts.json");
 
-// TODO: задокументувати кожну функцію
-function listContacts() {
-    // ...твій код
+const updateContacts = async (contacts) =>
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+
+const listContacts = async () => {
+  const data = await fs.readFile(contactsPath);
+
+  // console.log(data.toString())
+  // console.log(JSON.parse(data))
+
+  // const data = await fs.readFile(contactsPath, "utf-8")
+  // console.log(data)
+
+  return JSON.parse(data);
+};
+
+const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find((item) => item.id === contactId);
+  return result || null;
+};
+
+const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
   }
-  
-  function getContactById(contactId) {
-    // ...твій код
-  }
-  
-  function removeContact(contactId) {
-    // ...твій код
-  }
-  
-  function addContact(name, email, phone) {
-    // ...твій код
-  }
+  const [result] = contacts.splice(index, 1);
+  await updateContacts(contacts);
+  return result;
+};
+
+const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+
+  contacts.push(newContact);
+  await updateContacts(contacts);
+  return newContact;
+};
+
+module.exports = {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
